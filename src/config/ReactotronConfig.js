82208c9ace46f4c1d19023818f34f014aa3edfec
rafaelreisramos <rrramos@gmail.com>
2019-10-18 16:16:36 -0300

    redux and react reactotron support added

---
+++ src/config/ReactotronConfig.js
@@ -0,0 +1,10 @@
+import Reactotron from 'reactotron-react-js';
+import { reactotronRedux } from 'reactotron-redux';
+
+if (process.env.NODE_ENV === 'development') {
+  const tron = Reactotron.configure({ host: '192.168.1.100' })
+    .use(reactotronRedux())
+    .connect();
+
+  console.tron = tron;
+}
