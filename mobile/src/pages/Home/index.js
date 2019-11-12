7c336a230ab52d0a4db79244b7b8936bd9faae84
rafaelreisramos <rrramos@gmail.com>
2019-11-12 15:50:06 -0300

    styled components

--- mobile/src/pages/Home/index.js
+++ mobile/src/pages/Home/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
-import { View } from 'react-native';
-
-// import { Container } from './styles';
+import { Container } from './styles';
 
 export default function Home() {
-  return <View />;
+  return <Container />;
 }
 
 Home.navigationOptions = {
