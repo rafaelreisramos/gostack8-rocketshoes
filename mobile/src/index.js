815a08fc97e474852fce00a2d1e3753b4208f11f
rafaelreisramos <rrramos@gmail.com>
2019-11-12 15:04:47 -0300

    status bar

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
+import { StatusBar } from 'react-native';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <>
+      <Routes />
+      <StatusBar barStyle="light-content" backgroundColor="#141419" />
+    </>
+  );
 }
