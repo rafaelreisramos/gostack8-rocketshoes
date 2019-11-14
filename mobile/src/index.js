b09609c8f1fefada4f3709851d406764349f1e28
rafaelreisramos <rrramos@gmail.com>
2019-11-14 20:20:44 -0300

    redux support added. Cart store created

--- mobile/src/index.js
+++ mobile/src/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
+import { Provider } from 'react-redux';
 import './config/ReactotronConfig';
 
 import Routes from './routes';
 
+import store from './store';
+
 export default function App() {
   return (
-    <>
+    <Provider store={store}>
       <Routes />
       <StatusBar barStyle="light-content" backgroundColor="#141419" />
-    </>
+    </Provider>
   );
 }
